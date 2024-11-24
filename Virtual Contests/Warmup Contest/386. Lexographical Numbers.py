b16af2116b19fbd4f5a7a69c3b453b13a4511727
Drishti Sharma <118450578+drishtiisharma@users.noreply.github.com>
2024-11-24 12:16:13 +0530

    Create 386. Lexographical Numbers.py

---
+++ Virtual Contests/Warmup Contest/386. Lexographical Numbers.py
@@ -0,0 +1,22 @@
+#Problem Statement:
+#Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+#You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
+#Difficulty level: Medium
+
+class Solution(object):
+    def lexicalOrder(self, n):
+        num = 1
+        ans = []
+        while len(ans) < n:
+            ans.append(num)
+
+            if num * 10 <= n: 
+                num *= 10
+            elif num % 10 != 9 and num + 1 <= n: 
+                num += 1
+            else:
+                while (num / 10) % 10 == 9:
+                    num /= 10
+                num = (num / 10) + 1
+                
+        return ans
