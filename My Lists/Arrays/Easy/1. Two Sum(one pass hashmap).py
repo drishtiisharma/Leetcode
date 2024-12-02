c1c606c5064b0bf28daf36eea3aa2badc721fc5d
Drishti Sharma <118450578+drishtiisharma@users.noreply.github.com>
2024-12-02 19:07:00 +0530

    Create 1. Two Sum(one pass hashmap).py

---
+++ My Lists/Arrays/Easy/1. Two Sum(one pass hashmap).py
@@ -0,0 +1,13 @@
+#using one pass hashmap method
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmp={}
+        n=len(nums)
+
+        #using one pass hash table
+        for i in range(n):
+            complement=target-nums[i]
+            if complement in hashmp:
+                return [hashmp[complement],i]
+            hashmp[nums[i]]=i
